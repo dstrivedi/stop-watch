c26846f8398d0862d703a98e1089fb0a341ab366
dstrivedi <drashti1118@gmail.com>
2020-09-05 19:08:22 +0530

    2 files modified

--- index.js
+++ index.js
@@ -3,4 +3,22 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+var start = document.getElementById('startBtn');
+var stop = document.getElementById('stopBtn');
+var reset = document.getElementById('resetBtn');
+
+var secs = parseInt(document.getElementById('seconds').innerHTML);
+var mins = parseInt(document.getElementById('mins').innerHTML);
+
+start.addEventListener('click', function() {
+
+});
+
+stop.addEventListener('click', function() {
+
+});
+
+reset.addEventListener('click', function() {
+
+});
+

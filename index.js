9ea106ca9f20c3d0f0666e9a0f78263c95d04efd
dstrivedi <drashti1118@gmail.com>
2020-09-07 14:52:52 +0530

    2 files modified

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const appDiv = document.getElementById("app");
 var start = document.getElementById("startBtn");
 var stop = document.getElementById("stopBtn");
 var reset = document.getElementById("resetBtn");
+var secs = document.getElementById("seconds");
+var min = document.getElementById("mins");
+var millisecs = document.getElementById("milliseconds");
+var hr = document.getElementById("hour");
 var milliseconds = parseInt("00", 8);
 var hour = parseInt("00", 8);
 var seconds = parseInt("00", 8);
@@ -14,22 +18,36 @@ var timer;
 
 function startTimer() {
   milliseconds++;
-  formatTime(milliseconds);
+  formatTime(milliseconds, millisecs);
 
   if(milliseconds >= 100) {
     seconds++;
     milliseconds = "00";
     document.getElementById("milliseconds").innerHTML = milliseconds;
-    formatTime(seconds);
+    formatTime(seconds, secs);
+  }
+
+  if(seconds >= 60) {
+    minutes++;
+    seconds = "00";
+    document.getElementById("seconds").innerHTML = seconds;
+    formatTime(minutes, min);
+  }
+
+  if(minutes >= 60) {
+    hour++;
+    minutes = "00";
+    document.getElementById("hour").innerHTML = hour;
+    formatTime(hour, hr);
   }
  
 }
 
-function formatTime(unit) {
-  if(unit <= 0) {
-    document.getElementById(unit).innerHTML = "0" + unit;
+function formatTime(unit, form) {
+  if(unit <= 9) {
+    form.innerHTML = "0" + unit;
   } else {
-    document.getElementById(unit).innerHTML = unit;
+    form.innerHTML = unit;
   }
 }
 
